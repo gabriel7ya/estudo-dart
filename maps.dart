94c04b987209c640814654999b2d165faf33662b
gabriel7ya <gabriel7ya@gmail.com>
2021-01-22 23:55:53 -0300

    Sobre maps e generics

---
+++ maps.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+List<Map<String, dynamic>> cadastros = [];
+Map<String, dynamic> cadastro = {};
+
+main() {
+  print('Digite seu nome');
+  String nome = stdin.readLineSync();
+  cadastro['nome'] = nome;
+
+  print('Digite sua idade');
+  String idade = stdin.readLineSync();
+  cadastro['idade'] = idade;
+
+  print('Digite sua cidade');
+  String cidade = stdin.readLineSync();
+  cadastro['cidade'] = cidade;
+
+  print('Digite seu estado');
+  String estado = stdin.readLineSync();
+  cadastro['estado'] = estado;
+
+  cadastros.add(cadastro);
+  cadastros.add(cadastro);
+  print(cadastros);
+}
